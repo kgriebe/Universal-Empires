11640dfc308a1d823c9031dbee7228ee9d5b1446
Kris Griebe <kris@kingofweb.com>
2013-01-30 10:59:08 -0800

    refactored ships_json_generator to now function correctly

--- Data/ships_json_generator.py
+++ Data/ships_json_generator.py
@@ -15,7 +15,6 @@ LBS_ship_dict = {}
 DIC_ship_dict = {}
 STS_ship_dict = {}
 ships_attrs = {}
-ship_attrs_json = {}
 
 # Create the dictionaries by reading from a manually created file housing a list of ships with
 # identity numbers and rarity numbers.  Sort the file into a dictionary for each race.
@@ -39,69 +38,54 @@ def dict_creator(file):
 # Update the values of the dictionary so it has the ship name, rarity, and identity number.
 # Json format the string and write it to the appropriate ship file.
 def dict_update():
-	# Define default attributes
-	ship_attrs = {"name": "Default", "exp": 0, "level": 0, "max_level": 0, "ident": 0, 
-	"rarity": 0, "shield_points": 0, "repair_rate": 0, "damage_reduction": 0, 
-	"damage_reduction_type": 0, "attack_value": 0, "speed": 0, "skill": 0, "weapon_mod": 0, 
-	"shield_mod": 0, "engine_mod": 0, "damage_type": 0, "weapon_hardpoint": 0, 
-	"armor_hardpoint": 0, "shield_hardpoint": 0, "engine_hardpoint": 0, "fleet_leader_skill": ""}
+    	# Define default attributes
+    	RUFS_ships = {}
+    	AMV_ships = {}
+    	LBS_ships = {}
+    	DIC_ships = {}
+    	STS_ships = {}
+    	
+    	ship_attrs_base = {"exp": 0, "level": 0, "max_level": 0, "shield_points": 0, "repair_rate": 0, 
+    "damage_reduction": 0, "damage_reduction_type": 0, "attack_value": 0, "speed": 0, "skill": 0, 
+    "weapon_mod": 0, "shield_mod": 0, "engine_mod": 0, "damage_type": 0, "weapon_hardpoint": 0, 
+    "armor_hardpoint": 0, "shield_hardpoint": 0, "engine_hardpoint": 0, "fleet_leader_skill": ""}
 
-	""" Iterate over the ship dictionaries, convert the value tuple (containing id number and
-		rarity number) into a list so it can be accessed via index.  Format json and assign it
-		variable, then write it to file"""
-	for ship, value in RUFS_ship_dict.iteritems():
-		attribute_list = list(value)
-		ship_attrs = {"name": ship, "exp": 0, "level": 0, "max_level": 0, "ident": attribute_list[1], 
-	"rarity": attribute_list[0], "shield_points": 0, "repair_rate": 0, "damage_reduction": 0, 
-	"damage_reduction_type": 0, "attack_value": 0, "speed": 0, "skill": 0, "weapon_mod": 0, 
-	"shield_mod": 0, "engine_mod": 0, "damage_type": 0, "weapon_hardpoint": 0, 
-	"armor_hardpoint": 0, "shield_hardpoint": 0, "engine_hardpoint": 0, "fleet_leader_skill": ""}
-		string = json.dumps({attribute_list[1]:ship_attrs})
-		RUFS_ship_file.write(string + "," + "\n")
-
-	for ship, value in AMV_ship_dict.iteritems():
-		attribute_list = list(value)
-		ship_attrs = {"name": ship, "exp": 0, "level": 0, "max_level": 0, "ident": attribute_list[1], 
-	"rarity": attribute_list[0], "shield_points": 0, "repair_rate": 0, "damage_reduction": 0, 
-	"damage_reduction_type": 0, "attack_value": 0, "speed": 0, "skill": 0, "weapon_mod": 0, 
-	"shield_mod": 0, "engine_mod": 0, "damage_type": 0, "weapon_hardpoint": 0, 
-	"armor_hardpoint": 0, "shield_hardpoint": 0, "engine_hardpoint": 0, "fleet_leader_skill": ""}
-		string = json.dumps({attribute_list[1]:ship_attrs})
-		AMV_ship_file.write(string + "," + "\n")
-
-	for ship, value in LBS_ship_dict.iteritems():
-		attribute_list = list(value)
-		ship_attrs = {"name": ship, "exp": 0, "level": 0, "max_level": 0, "ident": attribute_list[1], 
-	"rarity": attribute_list[0], "shield_points": 0, "repair_rate": 0, "damage_reduction": 0, 
-	"damage_reduction_type": 0, "attack_value": 0, "speed": 0, "skill": 0, "weapon_mod": 0, 
-	"shield_mod": 0, "engine_mod": 0, "damage_type": 0, "weapon_hardpoint": 0, 
-	"armor_hardpoint": 0, "shield_hardpoint": 0, "engine_hardpoint": 0, "fleet_leader_skill": ""}
-		string = json.dumps({attribute_list[1]:ship_attrs})
-		LBS_ship_file.write(string + "," + "\n")
-
-	for ship, value in DIC_ship_dict.iteritems():
-		attribute_list = list(value)
-		ship_attrs = {"name": ship, "exp": 0, "level": 0, "max_level": 0, "ident": attribute_list[1], 
-	"rarity": attribute_list[0], "shield_points": 0, "repair_rate": 0, "damage_reduction": 0, 
-	"damage_reduction_type": 0, "attack_value": 0, "speed": 0, "skill": 0, "weapon_mod": 0, 
-	"shield_mod": 0, "engine_mod": 0, "damage_type": 0, "weapon_hardpoint": 0, 
-	"armor_hardpoint": 0, "shield_hardpoint": 0, "engine_hardpoint": 0, "fleet_leader_skill": ""}
-		string = json.dumps({attribute_list[1]:ship_attrs})
-		DIC_ship_file.write(string + "," + "\n")
-
-	for ship, value in STS_ship_dict.iteritems():
-		attribute_list = list(value)
-		ship_attrs = {"name": ship, "exp": 0, "level": 0, "max_level": 0, "ident": attribute_list[1], 
-	"rarity": attribute_list[0], "shield_points": 0, "repair_rate": 0, "damage_reduction": 0, 
-	"damage_reduction_type": 0, "attack_value": 0, "speed": 0, "skill": 0, "weapon_mod": 0, 
-	"shield_mod": 0, "engine_mod": 0, "damage_type": 0, "weapon_hardpoint": 0, 
-	"armor_hardpoint": 0, "shield_hardpoint": 0, "engine_hardpoint": 0, "fleet_leader_skill": ""}
-		string = json.dumps({attribute_list[1]:ship_attrs})
-		STS_ship_file.write(string + "," + "\n")
+    	""" Iterate over key:value pairs in dicts created by dict_creator, then create new dict containing the base attributes plus
+    	newly defined attributes, then insert that into a new dictionary with the id number as the key for each object.  Then convert
+    	to json and dump to file."""
+    	for ship_name, rare_id in RUFS_ship_dict.iteritems():
+    		ship_attrs = dict(ship_attrs_base.items() + [('name', ship_name), ('ident', rare_id[1]), ('rarity', rare_id[0])])
+    		RUFS_ships[rare_id[1]] = ship_attrs
+    	string = json.dumps(RUFS_ships)
+    	RUFS_ship_file.write(string)
+    
+        for ship_name, rare_id in AMV_ship_dict.iteritems():
+    		ship_attrs = dict(ship_attrs_base.items() + [('name', ship_name), ('ident', rare_id[1]), ('rarity', rare_id[0])])
+    		AMV_ships[rare_id[1]] = ship_attrs
+    	string = json.dumps(AMV_ships)
+    	AMV_ship_file.write(string)
+    	
+    	for ship_name, rare_id in LBS_ship_dict.iteritems():
+    		ship_attrs = dict(ship_attrs_base.items() + [('name', ship_name), ('ident', rare_id[1]), ('rarity', rare_id[0])])
+    		LBS_ships[rare_id[1]] = ship_attrs
+    	string = json.dumps(LBS_ships)
+    	LBS_ship_file.write(string)
+    	
+    	for ship_name, rare_id in DIC_ship_dict.iteritems():
+    		ship_attrs = dict(ship_attrs_base.items() + [('name', ship_name), ('ident', rare_id[1]), ('rarity', rare_id[0])])
+    		DIC_ships[rare_id[1]] = ship_attrs
+    	string = json.dumps(DIC_ships)
+    	DIC_ship_file.write(string)
+    	
+    	for ship_name, rare_id in STS_ship_dict.iteritems():
+    		ship_attrs = dict(ship_attrs_base.items() + [('name', ship_name), ('ident', rare_id[1]), ('rarity', rare_id[0])])
+    		STS_ships[rare_id[1]] = ship_attrs
+    	string = json.dumps(STS_ships)
+    	STS_ship_file.write(string)
 
 def main():
+	
 	dict_creator("Ships.txt")
-
 	dict_update()
 	STS_ship_file.close()
 	DIC_ship_file.close()
